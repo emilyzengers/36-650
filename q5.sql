5c3007a069f2a5c565edcb92fbc18ff724d307b8
emilyzengers <69126128+emilyzengers@users.noreply.github.com>
2021-09-25 19:01:55 -0400

    Delete q5.sql

--- q5.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO rdata_with_constraint (a,b,x) VALUES
-('thumb', 'teeth', -55.2);
-
-INSERT INTO rdata_with_constraint (a,b,x) VALUES
-('thumb', 'teeth', 55.2);
-
-SELECT * FROM rdata_with_constraint;
